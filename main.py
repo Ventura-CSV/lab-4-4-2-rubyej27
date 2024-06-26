a838e168a260b6f271ec1b060d30e467c9de6543
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-06-26 11:03:23 -0400

    4-4-2 commit

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    while True:
+        try:
+            number = int(input('Enter a number: '))
+        except ValueError:
+            print ('Invalid input: Value Error')
+            continue
+        else:
+            print (number)
+            break
 
     ########################################
     # Do not delete the return statement
